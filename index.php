f25f0fea4263d59863d494d2d8cfa1eb03324643
Raphael Pena <raphael.pena@fadesp.org.br>
2025-07-09 16:54:02 -0300

    Inicio dos estudos

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+  <header>
+    <nav>
+      <a href="">Link 1</a>
+      <a href="">Link 2</a>
+      <a href="">Link 3</a>
+    </nav>
+  </header>
+
+  <main>
+    <div>
+      <div>
+        <img src="" alt="">
+      </div>
+      <div>
+        <h1>Nome</h1>
+        <sub>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum fugiat vitae exercitationem vel molestias sequi id! Doloribus placeat quam possimus minus iste ipsum, quis tenetur aliquam accusantium accusamus, perferendis labore?</sub>
+      </div>
+    </div>
+  </main>
+
+  <footer>
+
+  </footer>
+</body>
+
+</html>
